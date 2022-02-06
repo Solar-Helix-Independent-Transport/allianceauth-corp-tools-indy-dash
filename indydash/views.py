b5ad838d3bef35979e6523d0daca499731ffbced
Aaron Kable <aaronkable@gmail.com>
2022-02-06 08:25:19 +0800

    . not :

--- indydash/views.py
+++ indydash/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 
 
 @login_required
-@permission_required('indydash:view_dash')
+@permission_required('indydash.view_dash')
 def react_bootstrap(request):
     return render(request, 'indydash/react_base.html')
